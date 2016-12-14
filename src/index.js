e4961c47248b21d2f9560d7d494e6633c15994c0
LeuisKen <leuisken@gmail.com>
2016-12-14 17:11:30 +0800

    update code style

--- src/index.js
+++ src/index.js
@@ -1,100 +1,107 @@
-import './asserts/styles/iconfont.css'
-import './asserts/styles/index.less'
-import Model from './model'
+/**
+ * @file 核心业务逻辑
+ * @author Leuis Ken <leuisken@gmail.com>
+ */
+import './asserts/styles/iconfont.css';
+import './asserts/styles/index.less';
+import Model from './model';
 
-const $ = document.querySelector.bind(document)
+const $ = document.querySelector.bind(document);
 
 const View = {
-  render(data) {
-    let frag = document.createDocumentFragment(),
-        list = $('.account-list')
-    Object.keys(data).forEach(el => {
-      let item = document.createElement('li'),
-          date = new Date(+el)
-      item.className = 'account-list-item'
-      item.innerHTML = `
-        <span>${data[el].type}</span>
-        <span class="timestamp">${date.getFullYear() % 100}/${date.getMonth() + 1}/${date.getDate()}</span>
-        <span class="amount ${data[el].amount.toString()[0] !== '-' ? 'income' : 'cost'}">${data[el].amount}</span>
-        <button>删除</button>
-      `
-      frag.appendChild(item)
-    })
-    list.innerHTML = ''
-    list.appendChild(frag)
-  }
-}
-
-Model.onchange = function(data) {
-  View.render(data)
-}
-
-let takeNoteBth = $('#take-note'),
-    amount = $('#amount'),
-    list = $('.account-list'),
-    addBtn = $('.add-account'),
-    modal = $('.modal'),
-    modalClose = $('.modal .close')
-
-takeNoteBth.addEventListener('click', function(e) {
-  let type = $('#type').value
-
-  Model.update(+new Date, {
-    type,
-    amount: type === '收入' ? +amount.value : -amount.value
-  })
-  amount.value = ''
-})
-
-amount.addEventListener('keydown', function(e) {
-  if (e.keyCode === 13) {
-    takeNoteBth.click()
-  }
-})
-
-list.addEventListener('click', function({ target }) {
-  if (target.textContent === '删除') {
-    let timestamp = target.parentNode.querySelector('.timestamp').textContent
-    Model.delete(timestamp)
-  }
-})
-
-list.addEventListener('touchstart', function({ target }) {
-  let element = target
-  while (element.nodeName !== 'LI' || element.className !== 'account-list-item') {
-    if (element === this) {
-      return
+    render(data) {
+        let frag = document.createDocumentFragment();
+        let list = $('.account-list');
+        Object.keys(data).forEach(el => {
+            let item = document.createElement('li');
+            let date = new Date(+el);
+            item.className = 'account-list-item';
+            item.innerHTML = `
+                <span>${data[el].type}</span>
+                <span class="timestamp">${date.getFullYear() % 100}/${date.getMonth() + 1}/${date.getDate()}</span>
+                <span class="amount ${data[el].amount.toString()[0] !== '-' ? 'income' : 'cost'}">
+                    ${data[el].amount}
+                </span>
+                <button>删除</button>
+            `;
+            frag.appendChild(item);
+        });
+        list.innerHTML = '';
+        list.appendChild(frag);
     }
-    element = element.parentNode
-  }
-  console.log(element)
-})
+};
 
-addBtn.addEventListener('click', function({ target }) {
-  window.location.hash = 'add'
-})
+Model.onchange = function (data) {
+    View.render(data);
+};
 
-window.addEventListener('hashchange', function() {
+let takeNoteBth = $('#take-note');
+let amount = $('#amount');
+let list = $('.account-list');
+let addBtn = $('.add-account');
+let modal = $('.modal');
+let modalClose = $('.modal .close');
+
+takeNoteBth.addEventListener('click', function (e) {
+    let type = $('#type').value;
+
+    Model.update(+new Date(), {
+        type,
+        amount: type === '收入' ? +amount.value : -amount.value
+    });
+    amount.value = '';
+});
+
+amount.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        takeNoteBth.click();
+    }
+});
+
+list.addEventListener('click', function ({target}) {
+    if (target.textContent === '删除') {
+        let timestamp = target.parentNode.querySelector('.timestamp').textContent;
+        Model.delete(timestamp);
+    }
+});
+
+list.addEventListener('touchstart', function ({target}) {
+    let element = target;
+    while (element.nodeName !== 'LI' || element.className !== 'account-list-item') {
+        if (element === this) {
+            return;
+        }
+        element = element.parentNode;
+    }
+    console.log(element);
+});
+
+addBtn.addEventListener('click', function ({target}) {
+    window.location.hash = 'add';
+});
+
+window.addEventListener('hashchange', function () {
     if (window.location.hash === '#add') {
-        modal.style.visibility = 'visible'
-        modal.style.opacity = 1
-    } else {
-        modal.style.opacity = 0
+        modal.style.visibility = 'visible';
+        modal.style.opacity = 1;
     }
-})
+    else {
+        modal.style.opacity = 0;
+    }
+});
 
-modal.addEventListener('transitionend', function({ propertyName }) {
+modal.addEventListener('transitionend', function ({propertyName}) {
     if (propertyName === 'opacity' && this.style.opacity === '0') {
-        this.style.visibility = 'hidden'
+        this.style.visibility = 'hidden';
     }
-})
+});
 
-modalClose.addEventListener('click', function() {
-    window.location.hash = ''
-})
+modalClose.addEventListener('click', function () {
+    window.location.hash = '';
+});
 
-window.addEventListener('load', function() {
-    window.dispatchEvent(new Event('hashchange'))
-})
+window.addEventListener('load', function () {
+    window.dispatchEvent(new Event('hashchange'));
+});
 
-View.render(Model.get())
+View.render(Model.get());
--- src/model.js
+++ src/model.js
@@ -1,28 +1,32 @@
-let data = JSON.parse(window.localStorage.getItem('note'))
+/**
+ * @file 数据模型操作
+ * @author Leuis Ken <leuisken@gmail.com>
+ */
+let data = JSON.parse(window.localStorage.getItem('note'));
 
 if (data === null) {
-  data = {}
-  window.localStorage.setItem('note', JSON.stringify(data))
+    data = {};
+    window.localStorage.setItem('note', JSON.stringify(data));
 }
 
 export default {
-  get() {
-    return data
-  },
-  update(key, value, callback) {
-    data[key] = value
-    this.sync()
-    this.onchange(data)
-  },
-  delete(key) {
-    delete data[key]
-    this.sync()
-    this.onchange(data)
-  },
-  sync() {
-    window.localStorage.setItem('note', JSON.stringify(data))
-  },
-  onchange(data) {
+    get() {
+        return data;
+    },
+    update(key, value, callback) {
+        data[key] = value;
+        this.sync();
+        this.onchange(data);
+    },
+    delete(key) {
+        delete data[key];
+        this.sync();
+        this.onchange(data);
+    },
+    sync() {
+        window.localStorage.setItem('note', JSON.stringify(data));
+    },
+    onchange(data) {
 
-  }
-}
+    }
+};
